2dab85225b6b728491cc133302abd919c9778cb2
ZelonOfficial <Coolishsilver86@gmail.com>
2021-12-16 10:58:44 +0100

    ćwiczenie opisujące oop

---
+++ OOP1.py
@@ -0,0 +1,21 @@
+#OBIEKT, WARTOSC, ZMIENNA
+a = 1
+print(id(a))
+
+a = 2
+
+print(id(a))
+
+b = a
+
+print(id(b))
+
+print(type(a))
+
+print(dir(a))
+
+def mojafoo():
+    return 10
+
+print("\n--------------------------------------------------------------------------------\n")
+print(dir(mojafoo))
